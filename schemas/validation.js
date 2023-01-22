d9f19f78c1abe7761d089c4700b6fb7bcccbfe34
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-01-22 14:18:44 +0200

    Update validation.js

--- schemas/validation.js
+++ schemas/validation.js
@@ -17,4 +17,19 @@ module.exports = {
 
     next();
   },
+  putContactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().alphanum().min(3).max(30).optional(),
+      email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+        .optional(),
+      phone: Joi.number().optional(),
+    });
+
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+      return res.status(400).json({ message: validationResult.error });
+    }
+    next();
+  },
 };
