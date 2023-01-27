376b2d596e91ef0de6e1cf4ba113ca307b9eb220
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-01-27 15:21:09 +0200

    Update getAllContacts.js

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -1,7 +1,7 @@
 const Contact = require("../../models/contact");
 
 const getAllContacts = async (_, res) => {
-  const contacts = await Contact.find();
+  const contacts = await Contact.find({}, "-createdAt -updatedAt");
   res.json(contacts);
 };
 
