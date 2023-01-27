5bdef58fef57e6afbc4731cafe971d4fd9e48dba
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-01-27 15:21:13 +0200

    Update getContactById.js

--- controllers/contacts/getContactById.js
+++ controllers/contacts/getContactById.js
@@ -2,8 +2,11 @@ const Contact = require("../../models/contact");
 
 const getContactById = async (req, res) => {
   const { contactId } = req.params;
-  const contact = await Contact.findById(contactId);
-
+  //const contact = await Contact.findById(contactId);
+  const contact = await Contact.findOne(
+    { _id: contactId },
+    "-createdAt -updatedAt"
+  );
   if (!contact) {
     res.status(404).json({ message: "Not found" });
     return;
