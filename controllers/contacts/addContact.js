2d6b235863e4e80a92791e7fa8812d1681407736
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-01-28 13:02:42 +0200

    Update addContact.js

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,14 +1,8 @@
 const Contact = require("../../models/contact");
-const { addContactsSchema } = require("../../schema/validation");
 
 const addContact = async (req, res) => {
-  const { error } = addContactsSchema.validate(req.body);
-  if (error) {
-    res.status(400).json({ message: error.message });
-    return;
-  }
   const result = await Contact.create(req.body);
-  res.status(201).json(result);
+  res.status(201).json({ code: 201, result });
 };
 
 module.exports = addContact;
