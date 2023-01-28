26c078dd5360bb894c28f44ea8ac514f690d99c5
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-01-28 13:05:44 +0200

    Update updateContactById.js

--- controllers/contacts/updateContactById.js
+++ controllers/contacts/updateContactById.js
@@ -1,18 +1,12 @@
 const Contact = require("../../models/contact");
-const { putContactsSchema } = require("../../schema/validation");
 
 const updateContactById = async (req, res) => {
-  const { error } = putContactsSchema.validate(req.body);
-  if (error) {
-    res.status(400).json({ message: "Bad Request" });
-    return;
-  }
   const { contactId } = req.params;
   const result = await Contact.findByIdAndUpdate(contactId, req.body, {
     new: true,
   });
   if (!result) {
-    res.status(404).json({ message: "Not found" });
+    res.status(404).json({ code: 404, message: "Not found" });
     return;
   }
   res.json(result);
