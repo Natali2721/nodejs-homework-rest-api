24e76b563591896a36e12377337db3e12e2d7395
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-01-28 13:02:11 +0200

    Update validation.js

--- schema/validation.js
+++ schema/validation.js
@@ -1,23 +1,50 @@
 const Joi = require("joi");
 
-const addContactsSchema = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
-  phone: Joi.number().min(7).required(),
-  favorite: Joi.boolean(),
-});
-const putContactsSchema = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).optional(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .optional(),
-  phone: Joi.number().optional(),
-}).min(1);
+module.exports = {
+  addContactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().alphanum().min(3).max(30).required(),
+      email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+        .required(),
+      phone: Joi.number().min(7).required(),
+    });
 
-const updateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean().required(),
-});
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+      return res
+        .status(400)
+        .json({ code: 400, message: validationResult.error });
+    }
 
-module.exports = { addContactsSchema, putContactsSchema, updateFavoriteSchema };
+    next();
+  },
+  putContactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().alphanum().min(3).max(30).optional(),
+      email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+        .optional(),
+      phone: Joi.number().optional(),
+    });
+
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+      return res.status(400).json({ code: 400, message: "Bad Request" });
+    }
+    next();
+  },
+  updateFavoriteContactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      favorite: Joi.boolean().required(),
+    });
+
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+      return res
+        .status(400)
+        .json({ code: 400, message: validationResult.error });
+    }
+    next();
+  },
+};
