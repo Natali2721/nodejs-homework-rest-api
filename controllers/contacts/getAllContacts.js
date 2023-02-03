fca6dcbe36a89872454c2b34e3dcdba29a6e4001
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-02-03 21:41:34 +0200

    Update getAllContacts.js

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -4,10 +4,33 @@ const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
-  const contacts = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "name email");
+
+  //const contacts = await Contact.find({ owner }, "-createdAt -updatedAt", {
+  //  skip,
+  //  limit,
+  //}).populate("owner", "name email");
+  let contacts;
+  const { favorite } = req.query;
+  console.log(favorite);
+
+  if (!favorite) {
+    console.log("noFavoriteQuery".yellow);
+    contacts = await Contact.find({ owner }, "-createdAt -updatedAt", {
+      skip,
+      limit,
+    }).populate("owner", "name email");
+  } else {
+    console.log("noFavoriteQuery".yellow);
+    contacts = await Contact.find(
+      { owner, favorite },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit,
+      }
+    ).populate("owner", "name email");
+  }
+
   res.json(contacts);
 };
 
